061e2c36d5897e142d5bf2c59ef1d8c0765a95b0
phonglam <lamphong1507@gmail.com>
2024-07-13 18:19:49 +0700

    fix css

--- src/page/homePage.js
+++ src/page/homePage.js
@@ -1,42 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/system';
-import { Button } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/system";
+import { Button } from "@mui/material";
 
-const Root = styled('div')({
-  display: 'flex',
-  flexWrap: 'wrap',
-  justifyContent: 'space-around',
-  padding: '20px',
+const Root = styled("div")({
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "space-around",
+  padding: "20px",
 });
 
 const StyledCard = styled(Card)({
-  width: '300px',
-  margin: '10px',
+  width: "500px",
+  margin: "10px",
 });
 
 const StyledMedia = styled(CardMedia)({
-  height: 140,
+  height: 300,
 });
 
 const Home = () => {
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    axios.get('https://6680067356c2c76b495ae640.mockapi.io/studentManagement')
-      .then(response => {
-        const sortedStudents = response.data.sort((a, b) => a.name.localeCompare(b.name));
+    axios
+      .get("https://6680067356c2c76b495ae640.mockapi.io/studentManagement")
+      .then((response) => {
+        const sortedStudents = response.data.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
         //dong 33 la tuy theo de keu xu ly nhu the nao
         //Code hien tai la hien thi ten theo bang chu cai
         setStudents(sortedStudents);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
 
@@ -46,8 +49,8 @@ const Home = () => {
         Home Student
       </Typography>
       <Root>
-        {students.map(student => (
-          <div key={student.id}>
+        {students.map((student) => (
+          /* <div key={student.id}>
             <p>{student.id}</p>
             <p>{student.name}</p>
             <p>{student.dateofbirth}</p>
@@ -55,24 +58,39 @@ const Home = () => {
             <p>{student.class}</p>
            <img src={student.image}/>
           <Link to={`/detail/${student.id}`}> <Button variant='contained'>Detail</Button></Link>
-          </div>
+          </div> */
 
-          // <StyledCard key={section.id}>
-          //   <StyledMedia
-          //     image={section.image}
-          //     title={section.sectionName}
-          //   />
-          //   <CardContent>
-          //     <Link to={`/detail/${section.id}`} style={{ textDecoration: 'none' }}>
-          //       <Typography variant="h5" component="h2">
-          //         {section.sectionName}
-          //       </Typography>
-          //     </Link>
-          //     <Typography variant="body2" color="textSecondary" component="p">
-          //       Duration: {section.duration}
-          //     </Typography>
-          //   </CardContent>
-          // </StyledCard>
+          <StyledCard key={student.id}>
+            <StyledMedia image={student.image} title={student.name} />
+            <CardContent>
+              <Link
+                to={`/detail/${student.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Typography variant="h5" component="h2">
+                  {student.name}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {student.dateofbirth}{" "}
+                </Typography>
+                {student.gender ? (
+                  <Typography variant="h5" component="h2">
+                    Nam
+                  </Typography>
+                ) : (
+                  <Typography variant="h5" component="h2">
+                    Nữ
+                  </Typography>
+                )}
+                <Typography variant="h5" component="h2">
+                  {student.feedback}{" "}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {student.class}{" "}
+                </Typography>
+              </Link>
+            </CardContent>
+          </StyledCard>
         ))}
       </Root>
     </div>
--- src/page/sectionDetail.js
+++ src/page/sectionDetail.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/system';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/system";
 
 const StyledCard = styled(Card)({
   maxWidth: 345,
-  margin: '20px auto',
+  margin: "20px auto",
 });
 
 const StyledMedia = styled(CardMedia)({
@@ -21,12 +21,15 @@ const Detail = () => {
   const [student, setStudent] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://6680067356c2c76b495ae640.mockapi.io/studentManagement/${id}`)
-      .then(response => {
+    axios
+      .get(
+        `https://6680067356c2c76b495ae640.mockapi.io/studentManagement/${id}`
+      )
+      .then((response) => {
         setStudent(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, [id]);
 
@@ -35,31 +38,40 @@ const Detail = () => {
   }
 
   return (
-    <div>
-      <p>{student.name}</p>
-      <p>{student.dateofbirth}</p>
-      <p>{student.gender}</p>
-      <p>{student.class}</p>
-      <img src={student.image}/>
-      <p>{student.feedback}</p>
-    </div>
-    // <StyledCard>
-    //   <StyledMedia
-    //     image={section.image}
-    //     title={section.sectionName}
-    //   />
-    //   <CardContent>
-    //     <Typography gutterBottom variant="h5" component="h2">
-    //       {section.sectionName}
-    //     </Typography>
-    //     <Typography variant="body2" color="textSecondary" component="p">
-    //       Duration: {section.duration}
-    //     </Typography>
-    //     <Typography variant="body2" color="textSecondary" component="p">
-    //       {section.sectionDescription}
-    //     </Typography>
-    //   </CardContent>
-    // </StyledCard>
+    // <div>
+    //   <p>{student.name}</p>
+    //   <p>{student.dateofbirth}</p>
+    //   <p>{student.gender}</p>
+    //   <p>{student.class}</p>
+    //   <img src={student.image}/>
+    //   <p>{student.feedback}</p>
+    // </div>
+    <StyledCard>
+      <StyledMedia image={student.image} title={student.name} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {student.name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {student.dateofbirth}{" "}
+        </Typography>
+        {student.gender ? (
+          <Typography variant="body2" color="textSecondary" component="p">
+            Nam
+          </Typography>
+        ) : (
+          <Typography variant="body2" color="textSecondary" component="p">
+            Nữ
+          </Typography>
+        )}
+        <Typography variant="body2" color="textSecondary" component="p">
+          {student.class}{" "}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {student.feedback}{" "}
+        </Typography>
+      </CardContent>
+    </StyledCard>
   );
 };
 
